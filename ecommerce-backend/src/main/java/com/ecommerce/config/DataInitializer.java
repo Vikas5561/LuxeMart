117ddb6d800a9e5d53472cee63bcab18b90fdcba
Vikas5561 <vikasbarkade000@gmail.com>
2026-04-18 18:14:07 +0530

    Updated Configuration for Admin

--- ecommerce-backend/src/main/java/com/ecommerce/config/DataInitializer.java
+++ ecommerce-backend/src/main/java/com/ecommerce/config/DataInitializer.java
@@ -3,31 +3,33 @@ package com.ecommerce.config;
 import com.ecommerce.model.Admin;
 import com.ecommerce.repository.AdminRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class DataInitializer {
+@Component
+public class DataInitializer implements CommandLineRunner {
 
-    @Bean
-    CommandLineRunner initAdmin(AdminRepository adminRepository) {
-        return args -> {
+    private final AdminRepository adminRepository;
 
-            // Check if admin already exists
-            if (adminRepository.findByUsername("admin").isEmpty()) {
+    public DataInitializer(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
 
-                Admin admin = new Admin();
-                admin.setUsername("admin");
-                admin.setPassword("admin123"); // change later to encrypted
-                admin.setEmail("admin@luxemart.com");
-                admin.setRole("ADMIN");
+    @Override
+    public void run(String... args) {
 
-                adminRepository.save(admin);
+        if (adminRepository.findByUsername("admin").isEmpty()) {
 
-                System.out.println("✅ Admin created successfully!");
-            } else {
-                System.out.println("ℹ️ Admin already exists");
-            }
-        };
+            Admin admin = new Admin();
+            admin.setUsername("admin");
+            admin.setPassword("admin123");
+            admin.setEmail("admin@luxemart.com");
+            admin.setRole("ADMIN");
+
+            adminRepository.save(admin);
+
+            System.out.println("🔥🔥🔥 ADMIN CREATED SUCCESSFULLY 🔥🔥🔥");
+        } else {
+            System.out.println("ℹ️ Admin already exists");
+        }
     }
 }
